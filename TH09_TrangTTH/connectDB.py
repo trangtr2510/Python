8372f23a2714d0cac15b4032d6bc5028675d3604
trangtr2510 <trnnguyenthien12345@gmail.com>
2025-02-12 11:44:33 +0700

    add project

--- TH09_TrangTTH/connectDB.py
+++ TH09_TrangTTH/connectDB.py
@@ -1,9 +1,9 @@
 import mysql.connector
 
-conn = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="",
-    database="test"
-)
-cursor = conn.cursor()
+def connect_db():
+    return mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="",
+        database="test"
+    )
